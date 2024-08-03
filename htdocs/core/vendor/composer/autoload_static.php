fec3450ce2b423afcfaf15a2ed637be2daf3bdbd
xodivorce <prasidmandal79@gmail.com>
2024-08-03 22:05:58 +0530

    V9.7.0

--- htdocs/core/vendor/composer/autoload_static.php
+++ htdocs/core/vendor/composer/autoload_static.php
@@ -22,6 +22,7 @@ class ComposerStaticInit108be68e4e2b97fed51d36a10eed0849
         'P' => 
         array (
             'PhpOption\\' => 10,
+            'PHPMailer\\PHPMailer\\' => 20,
         ),
         'G' => 
         array (
@@ -49,6 +50,10 @@ class ComposerStaticInit108be68e4e2b97fed51d36a10eed0849
         'PhpOption\\' => 
         array (
             0 => __DIR__ . '/..' . '/phpoption/phpoption/src/PhpOption',
+        ),
+        'PHPMailer\\PHPMailer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
         ),
         'GrahamCampbell\\ResultType\\' => 
         array (
--- htdocs/home.php
+++ htdocs/home.php
@@ -1298,16 +1298,19 @@
 
   <section class="contact-form">
     <h3 class="h3 form-title">Contact Form</h3>
+
     <?php
+require './core/vendor/autoload.php'; // Adjust path as necessary
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 use Dotenv\Dotenv;
 
-// Load environment variables from .env file
-
+// Load environment variables
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+// Check if POST request
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $fullname = filter_var($_POST['fullname'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
@@ -1318,61 +1321,57 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email_restriction = explode(',', $_ENV['RESTRICTED_EMAILS']);
     $message_restriction = explode(',', $_ENV['RESTRICTED_MESSAGES']);
 
-    // Check if the email address is valid and not in the email restriction list
+    // Validation
     if (!filter_var($email, FILTER_VALIDATE_EMAIL) || in_array($email, $email_restriction)) {
-        echo "<script>alert('Whoopsie! 🐾 It seems there's a keyword restriction or the email address is playing hide-and-seek! 🕵️‍♂️ Please check the email address and try again ✨. Thanks a bunch!'); window.history.back();</script>";
-    } else {
-        // Check for restricted keywords in fullname
-        foreach ($name_restriction as $keyword) {
-            if (stripos($fullname, $keyword) !== false) {
-                echo "<script>alert('Whoopsie! 🐾 It seems your name contains restricted keywords.'); window.history.back();</script>";
-                exit;
-            }
+        echo "<script>alert('Whoopsie! 🐾 It seems there\'s a keyword restriction or the email address is playing hide-and-seek! 🕵️‍♂️ Please check the email address and try again ✨. Thanks a bunch!'); window.history.back();</script>";
+        exit;
+    }
+
+    foreach ($name_restriction as $keyword) {
+        if (stripos($fullname, $keyword) !== false) {
+            echo "<script>alert('Whoopsie! 🐾 It seems your name contains restricted keywords.'); window.history.back();</script>";
+            exit;
         }
+    }
 
-        // Check for restricted keywords in message
-        foreach ($message_restriction as $keyword) {
-            if (stripos($message, $keyword) !== false) {
-                echo "<script>alert('Whoopsie! 🐾 It seems your message contains restricted keywords! 🕵️‍♂️ Please check and try again ✨'); window.history.back();</script>";
-                exit;
-            }
+    foreach ($message_restriction as $keyword) {
+        if (stripos($message, $keyword) !== false) {
+            echo "<script>alert('Whoopsie! 🐾 It seems your message contains restricted keywords! 🕵️‍♂️ Please check and try again ✨'); window.history.back();</script>";
+            exit;
         }
+    }
 
-        $mail = new PHPMailer(true);
-        try {
-            // Server settings
-            $mail->isSMTP();
-            $mail->Host = $_ENV['SMTP_HOST'];
-            $mail->SMTPAuth = true;
-            $mail->Username = $_ENV['SMTP_USER'];
-            $mail->Password = $_ENV['SMTP_PASS'];
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-            $mail->Port = $_ENV['SMTP_PORT'];
+    $mail = new PHPMailer(true);
+    try {
+        $mail->isSMTP();
+        $mail->Host = $_ENV['SMTP_HOST'];
+        $mail->SMTPAuth = true;
+        $mail->Username = $_ENV['SMTP_USER'];
+        $mail->Password = $_ENV['SMTP_PASS'];
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port = $_ENV['SMTP_PORT'];
 
-            // Enable verbose debug output
-            $mail->SMTPDebug = 2; // 0 = off (for production use), 1 = commands only, 2 = data and commands
+        // Recipients
+        $mail->setFrom($_ENV['SMTP_USER'], 'Xodivorce Website');
+        $mail->addAddress($_ENV['SMTP_SENDTO']);
+        $mail->addReplyTo($email, $fullname);
 
-            // Recipients
-            $mail->setFrom($_ENV['SMTP_USER'], 'Xodivorce Website'); // Set the sender address
-            $mail->addAddress($_ENV['SMTP_SENDTO']); // Email address to receive the messages
-            $mail->addReplyTo($email, $fullname); // Add reply-to address
+        // Content
+        $mail->isHTML(true);
+        $mail->Subject = 'New Contact Form Submission';
+        $mail->Body    = "Ta-da! You've got a mail, from the contact form on your website.<br><br>" .
+                         "Full Name: " . $fullname . "<br>" .
+                         "Email: " . $email . "<br>" .
+                         "Message: <br>" . nl2br($message);
 
-            // Content
-            $mail->isHTML(true);
-            $mail->Subject = 'New Contact Form Submission';
-            $mail->Body    = "Ta-da! You've got a mail, from the contact form on your website.<br><br>" .
-                             "Full Name: " . $fullname . "<br>" .
-                             "Email: " . $email . "<br>" .
-                             "Message: <br>" . nl2br($message);
-
-            $mail->send();
-            echo "<script>alert('Success! Your message has taken off like a rocket! 🚀 We'll get back to you soon. Thanks for reaching out! ✨'); window.location.href = window.location.href;</script>";
-        } catch (Exception $e) {
-            echo "<script>alert('Whoopsie! 🐾 Our feedback feature seems to be chasing squirrels right now. Please reach out to us via email or try again later. 🌟 Mailer Error: {$mail->ErrorInfo}');</script>";
-        }
+        $mail->send();
+        echo "<script>alert('Success! Your message has taken off like a rocket! 🚀 We\'ll get back to you soon. Thanks for reaching out! ✨'); window.location.href = 'thankyou.php';</script>";
+    } catch (Exception $e) {
+        echo "<script>alert('Whoopsie! 🐾 Our feedback feature seems to be chasing squirrels right now. Please reach out to us via email or try again later. 🌟 Mailer Error: {$mail->ErrorInfo}');</script>";
     }
 }
 ?>
+
 
 
     <form action="" method="post" class="form" data-form onsubmit="return validateForm()">
