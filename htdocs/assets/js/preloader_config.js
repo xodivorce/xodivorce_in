2a52b8ce99db02b3be598622f3889bb8b2ded38f
xodivorce <prasidmandal79@gmail.com>
2025-03-17 16:31:23 +0530

    v9.14.9

--- htdocs/assets/js/preloader_config.js
+++ htdocs/assets/js/preloader_config.js
@@ -1,3 +1,9 @@
+//Controls the page preloader behaviour
+
 window.addEventListener("load", function () {
     document.querySelector(".preloader").style.display = "none";
+    document.body.style.overflow = "auto"; // Enable scrolling after loading
 });
+
+// Disable scrolling while preloader is active
+document.body.style.overflow = "hidden";
